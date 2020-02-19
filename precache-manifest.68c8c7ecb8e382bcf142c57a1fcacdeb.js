27c3f95ba2a9205ae5bb45ac18a505ca259bee07
Instinktaz <tomas.paskovskij@gmail.com>
2020-02-19 22:22:46 +0200

    Updates

---
+++ precache-manifest.68c8c7ecb8e382bcf142c57a1fcacdeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a805cffb3f8ad351f3335adbe68368d",
+    "url": "/react-api-cars/index.html"
+  },
+  {
+    "revision": "da6b0718fda9ce7a6e62",
+    "url": "/react-api-cars/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "570ec921b40b1c598c0b",
+    "url": "/react-api-cars/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "da6b0718fda9ce7a6e62",
+    "url": "/react-api-cars/static/js/2.f0cca932.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react-api-cars/static/js/2.f0cca932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570ec921b40b1c598c0b",
+    "url": "/react-api-cars/static/js/main.42c5a72f.chunk.js"
+  },
+  {
+    "revision": "e634b481492042f33484",
+    "url": "/react-api-cars/static/js/runtime-main.1f2d4af1.js"
+  }
+]);
